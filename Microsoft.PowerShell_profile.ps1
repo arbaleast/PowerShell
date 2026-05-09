d5fab424be6fce018c9c9328b4bde94ae1d5cbe9
arbaleast <3309839520@qq.com>
2026-05-09 17:30:17 +0800

    refactor: rename HermesRoot to UserScoop_ROOT for consistency

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,30 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 $OutputEncoding = [console]::InputEncoding
 $ProfileTimer = [System.Diagnostics.Stopwatch]::StartNew()
-$HermesRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$UserScoop_ROOT = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-. (Join-Path $HermesRoot "Config.ps1")
-. (Join-Path $HermesRoot "Utils.ps1")
-. (Join-Path $HermesRoot "Alias.ps1")
+. (Join-Path $UserScoop_ROOT "Config.ps1")
+. (Join-Path $UserScoop_ROOT "Utils.ps1")
+. (Join-Path $UserScoop_ROOT "Alias.ps1")
 
 # 懒加载：仅在首次调用时加载 Remote.ps1
-if (-not (Get-Command Start-TmuxSession -ErrorAction SilentlyContinue)) {
-    . (Join-Path $HermesRoot "Remote.ps1")
+if (-not (Get-Command Start-TmuxSession -ErrorAction SilentlyContinue))
+{
+    . (Join-Path $UserScoop_ROOT "Remote.ps1")
 }
 
-function sss {
+function sss
+{
     Start-TmuxSession @args
 }
 
-if (Get-Command Show-UserScoopLogo -ErrorAction SilentlyContinue) {
-    try { Show-UserScoopLogo } catch { }
+if (Get-Command Show-UserScoopLogo -ErrorAction SilentlyContinue)
+{
+    try
+    { Show-UserScoopLogo 
+    } catch
+    { 
+    }
 }
 
 $ProfileTimer.Stop()
